6055ad3dd8c515615cf76508aa707aa1f5ffccfb
Cann0u <raphael.dedack@epitech.eu>
2026-06-17 01:04:58 +0200

    [+] add  entity moove

--- game/entity.py
+++ game/entity.py
@@ -1,10 +1,15 @@
 import pygame
+from typing import Tuple
 
 
 class Entity:
-    def __init__(self, pos: tuple):
+    def __init__(self, pos: Tuple):
         self.pos = pos
-        self.move = [0, 0]
 
     def check_collapse(self):
         ...
+
+    def moove(self, dir: Tuple):
+        p_x, p_y = self.pos
+        d_x, d_y = dir
+        self.pos = d_x + p_x, d_y + p_y
--- game/pacman.py
+++ game/pacman.py
@@ -2,4 +2,6 @@ import pygame
 from entity import Entity
 
 class Pacman(Entity):
-    def __init__(self, ):
+    def __init__(self, pos, player: int):
+        super().__init__(pos)
+        self.player = player
