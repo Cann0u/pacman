bf3eaff64d35f4a4ff8728767e7670fffe39519e
HiiroKen <florian.morra@laposte.net>
2026-07-23 22:23:21 +0200

    Algo Ghosts

--- game/algo.py
+++ game/algo.py
@@ -1,11 +1,17 @@
-from abc import ABC, abstractmethod
 from math import inf
+
 
 class Algo:
     def __init__(self, map):
         self.map = map
 
-    def next_move(self, ghost_pos, target_pos):
+    def is_walkable(self, x, y):
+        maze = self.map.maze
+        if y < 0 or y >= len(maze) or x < 0 or x >= len(maze[0]):
+            return False
+        return maze[y][x] != "#"
+
+    def next_move(self, ghost_pos, target_pos, last_direction=None):
         g_x, g_y = ghost_pos
         t_x, t_y = target_pos
 
@@ -15,15 +21,19 @@ class Algo:
             "right": (g_x + 1, g_y),
             "left": (g_x - 1, g_y),
         }
+        opposite = {"up": "down", "down": "up", "left": "right", "right": "left"}
 
-        res = [None, inf]
+        best_direction, best_dist = None, inf
 
-        for direction, (g_x, g_y) in directions.items():
-            dist_sq = (g_x - t_x) ** 2 + (g_y - t_y) ** 2
-            print(direction, dist_sq)
-            
-            if dist_sq < res[1]:
-                res[1] = dist_sq
-                res[0] = direction
-                
-        return res[0]
+        for direction, (n_x, n_y) in directions.items():
+            if last_direction and direction == opposite[last_direction]:
+                continue
+            if not self.is_walkable(n_x, n_y):
+                continue
+
+            dist_sq = (n_x - t_x) ** 2 + (n_y - t_y) ** 2
+            if dist_sq < best_dist:
+                best_dist = dist_sq
+                best_direction = direction
+
+        return best_direction
